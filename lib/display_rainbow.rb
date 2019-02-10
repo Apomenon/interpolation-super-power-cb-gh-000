0c46ad8fa913126aab8e51f232ac3bd8ca706d79
Léa Weingärtner <nahyda@live.fr>
2019-02-10 09:30:42 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,5 +2,6 @@
 def display_rainbow
   color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 
+print "R : #{red}, O : #{orange}, Y : #{yellow}, G : #{green}, B : #{blue}, I : #{indigo}, V : #{violet}, "
 
 end
